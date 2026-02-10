528907106708fe3dcf63fd72e15df528a52d8f3b
Виктор <kasa.king_over@mail.ru>
2026-02-10 22:33:35 +0300

    Добавьте файлы проекта.

---
+++ Domain/Common/Enums/durationState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Enums
+{
+    public enum durationState
+    {
+        standart = 1,
+        shorting = 2
+    }
+}
---
+++ Domain/Common/Model/TargetEntity/DaySchedule/DayShceduleDTO.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common.Model.Common.Lesson;
+
+namespace Domain.Common.Model.TargetEntity.DaySchedule;
+
+public class DayShceduleDTO
+{
+    public string GroupName { get; set; } 
+    public DateOnly Date { get; set; }
+    public List<Lesson> Lessons { get; set; }
+}
