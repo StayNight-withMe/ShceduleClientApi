e07e11443bd43a5ee724498809052a08d4a92cab
Виктор <kasa.king_over@mail.ru>
2026-02-19 20:44:12 +0300

    feat(api): registration scalarUi

--- ClientScheduleApi/Program.cs
+++ ClientScheduleApi/Program.cs
@@ -1,19 +1,41 @@
-using Microsoft.AspNetCore.OpenApi;
+using ClientScheduleApi.Extensions.DI;
+using Infrastructure.DataBase.Context;
+using Scalar.AspNetCore;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+
+builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+
+builder.Services.AddCustomService();
+
+
+builder.Services.AddDataBaseDependency(builder.Configuration);
 
 var app = builder.Build();
 
+
+
 if (app.Environment.IsDevelopment())
 {
-    //app.MapOpenApi();
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
+
+app.MapCustomRoute();
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+
 
 app.MapControllers();
 
