27c1723d1104925794c1b77e204ffad8fe8fbc00
Виктор <kasa.king_over@mail.ru>
2026-02-20 09:16:27 +0300

    feet(context): create index for entity

--- Infrastructure/DataBase/Context/AppDbContext.cs
+++ Infrastructure/DataBase/Context/AppDbContext.cs
@@ -1,11 +1,20 @@
 ﻿
 
+using Domain.Model.Entities;
+using Domain.Model.Entity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.DataBase.Context
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<DayScheduleEntity> DaySchedule { get; set; }
+        public DbSet<LessonEntity> Lesson { get; set; }
+        public DbSet<TeacherEntity> Teacher { get; set; }
+        public DbSet<GroupEntity> Group { get; set; }
+        public DbSet<SpecialtyEntity> Specialty { get; set; }
+
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
@@ -15,6 +24,19 @@ namespace Infrastructure.DataBase.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<LessonEntity>()
+          .HasIndex(e => new { e.Teacher1, e.Teacher2 })
+          .HasDatabaseName("Teachers_Index");
+
+            modelBuilder.Entity<DayScheduleEntity>()
+            .HasIndex(e => new { e.Date })
+            .HasDatabaseName("Date_Index");
+
+            modelBuilder.Entity<DayScheduleEntity>()
+            .HasIndex(e => new { e.GroupName })
+            .HasDatabaseName("Group_Index");
+
         }
     }
 }
