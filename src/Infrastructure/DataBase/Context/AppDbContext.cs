f2f69222f49dc99e630c1f8e68ace9dacc5b505c
1111-ssss <gurskievgeni@gmail.com>
2026-02-27 12:24:14 +0300

    style(dbContext): fix tabulation and namespace style

--- src/Infrastructure/DataBase/Context/AppDbContext.cs
+++ src/Infrastructure/DataBase/Context/AppDbContext.cs
@@ -1,42 +1,36 @@
-﻿
-
-using Domain.Model.Entities;
+﻿using Domain.Model.Entities;
 using Domain.Model.Entity;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.DataBase.Context
+namespace Infrastructure.DataBase.Context;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
+    public DbSet<DayScheduleEntity> DaySchedule { get; set; }
+    public DbSet<LessonEntity> Lesson { get; set; }
+    public DbSet<TeacherEntity> Teacher { get; set; }
+    public DbSet<GroupEntity> Group { get; set; }
+    public DbSet<SpecialtyEntity> Specialty { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        public DbSet<DayScheduleEntity> DaySchedule { get; set; }
-        public DbSet<LessonEntity> Lesson { get; set; }
-        public DbSet<TeacherEntity> Teacher { get; set; }
-        public DbSet<GroupEntity> Group { get; set; }
-        public DbSet<SpecialtyEntity> Specialty { get; set; }
 
+    }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
-        }
+        modelBuilder.Entity<LessonEntity>()
+            .HasIndex(e => new { e.Teacher1, e.Teacher2 })
+            .HasDatabaseName("Teachers_Index");
 
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<LessonEntity>()
-          .HasIndex(e => new { e.Teacher1, e.Teacher2 })
-          .HasDatabaseName("Teachers_Index");
-
-            modelBuilder.Entity<DayScheduleEntity>()
+        modelBuilder.Entity<DayScheduleEntity>()
             .HasIndex(e => new { e.Date })
             .HasDatabaseName("Date_Index");
 
-            modelBuilder.Entity<DayScheduleEntity>()
+        modelBuilder.Entity<DayScheduleEntity>()
             .HasIndex(e => new { e.GroupName })
             .HasDatabaseName("Group_Index");
-
-        }
     }
 }
