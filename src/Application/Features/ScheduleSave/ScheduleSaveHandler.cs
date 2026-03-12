67112368c7e6298a0842ee98d320278ac234f915
Виктор <kasa.king_over@mail.ru>
2026-03-12 11:09:21 +0300

    refactor(app): add dependisy for handler

--- src/Application/Features/ScheduleSave/ScheduleSaveHandler.cs
+++ src/Application/Features/ScheduleSave/ScheduleSaveHandler.cs
@@ -1,5 +1,7 @@
 ﻿
+using Application.Abstraction.DataBase;
 using Contracts.Schedules;
+using Domain.Model.Entity;
 using MediatR;
 using Microsoft.Extensions.Logging;
 
@@ -7,9 +9,19 @@ namespace Application.Features.ScheduleSave;
 public class ScheduleSaveHandler : IRequestHandler<DayScheduleDTO>
 {
     public readonly ILogger<ScheduleSaveHandler> _logger;
-    public ScheduleSaveHandler(ILogger<ScheduleSaveHandler> logger)
+
+    public readonly IBaseRepository<DayScheduleEntity> _dayScheduleRepository;
+
+    public readonly IBaseRepository<LessonEntity> _lessonEntityRepository;
+
+    public ScheduleSaveHandler(
+        ILogger<ScheduleSaveHandler> logger,
+        IBaseRepository<DayScheduleEntity> dayScheduleRepository,
+        IBaseRepository<LessonEntity> lessonEntityRepository)
     {
         _logger = logger;
+        _dayScheduleRepository = dayScheduleRepository;
+        _lessonEntityRepository = lessonEntityRepository;
     }
 
     public Task Handle(DayScheduleDTO request, CancellationToken cancellationToken)
