c7cd90911e6ab51dd3980abaa3bd5684f5bf60bf
Виктор <kasa.king_over@mail.ru>
2026-03-12 08:32:24 +0300

    feet(db): create repository for feature

---
+++ src/Infrastructure/DataBase/Repository/Custom/CommonInfoRepository.cs
@@ -0,0 +1,34 @@
+﻿
+using Application.Abstraction.DataBase;
+using Ardalis.Specification;
+using Domain.Model.Entities;
+using Infrastructure.DataBase.Context;
+using Infrastructure.DataBase.Repository.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DataBase.Repository.Custom;
+
+public class CommonInfoRepository : BaseRepository<GroupEntity>, ICommonInfoRepository
+{
+    public CommonInfoRepository(AppDbContext AppDbContext) : base(AppDbContext) { }
+
+    public async Task<Dictionary<string, List<string>>> GetAllGroup() 
+    {
+
+        return await DbContext.Set<GroupEntity>()
+            .AsNoTracking()
+            .Include(c => c.Specialty)
+            .Where(c => c.Specialty != null)
+            .GroupBy(c => c.Specialty.Name)
+            .Select(c => new
+            {
+                SpecName = c.Key,
+                GroupNames = c.Select(x => x.Name).ToList()
+            })
+            .ToDictionaryAsync(c => c.SpecName, x => x.GroupNames);
+
+        
+    }
+
+
+}
