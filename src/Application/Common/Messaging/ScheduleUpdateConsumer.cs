11938d5ad52d3dda391d5f61463d782342a5a020
Виктор <kasa.king_over@mail.ru>
2026-03-14 08:45:37 +0300

    refactor(consumer): add logging and CancellationToken

--- src/Application/Common/Messaging/ScheduleUpdateConsumer.cs
+++ src/Application/Common/Messaging/ScheduleUpdateConsumer.cs
@@ -1,17 +1,25 @@
 ﻿using Contracts.Schedules;
 using MassTransit;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Common.Messaging;
 public class ScheduleUpdateConsumer : IConsumer<DayScheduleDTO>
 {
     private readonly ISender _mediator;
 
-    public ScheduleUpdateConsumer(ISender mediator) => _mediator = mediator;
-
+    private readonly ILogger<ScheduleUpdateConsumer> _logger;
+    public ScheduleUpdateConsumer(
+        ISender mediator,
+        ILogger<ScheduleUpdateConsumer> logger
+        )
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
     public async Task Consume(ConsumeContext<DayScheduleDTO> context)
     {
-        Console.WriteLine("консум client");
-        await _mediator.Send(context.Message); 
+        _logger.LogInformation("Consuming schedule update for group {Group}", context.Message.Group);
+        await _mediator.Send(context.Message, context.CancellationToken);
     }
 }
