c1c16c08a5e4297100ee7edcca494d06836bcd5f
Виктор <kasa.king_over@mail.ru>
2026-03-14 08:47:14 +0300

    feat(handler): implementation ScheduleSave

--- src/Application/Features/ScheduleSave/ScheduleSaveHandler.cs
+++ src/Application/Features/ScheduleSave/ScheduleSaveHandler.cs
@@ -1,6 +1,7 @@
-﻿
-using Application.Abstraction.DataBase;
+﻿using Application.Abstraction.DataBase;
+using AutoMapper;
 using Contracts.Schedules;
+using Domain.Common.CustomException;
 using Domain.Model.Entity;
 using MediatR;
 using Microsoft.Extensions.Logging;
@@ -8,25 +9,56 @@ using Microsoft.Extensions.Logging;
 namespace Application.Features.ScheduleSave;
 public class ScheduleSaveHandler : IRequestHandler<DayScheduleDTO>
 {
-    public readonly ILogger<ScheduleSaveHandler> _logger;
+    private readonly ILogger<ScheduleSaveHandler> _logger;
 
-    public readonly IBaseRepository<DayScheduleEntity> _dayScheduleRepository;
+    private readonly IBaseRepository<DayScheduleEntity> _dayScheduleRepository;
 
-    public readonly IBaseRepository<LessonEntity> _lessonEntityRepository;
+    private readonly IBaseRepository<LessonEntity> _lessonEntityRepository;
+
+    private readonly IMapper _mapper;
+
+    private readonly IUnitOfWork _unitOfWork;
 
     public ScheduleSaveHandler(
         ILogger<ScheduleSaveHandler> logger,
         IBaseRepository<DayScheduleEntity> dayScheduleRepository,
-        IBaseRepository<LessonEntity> lessonEntityRepository)
+        IBaseRepository<LessonEntity> lessonEntityRepository,
+        IUnitOfWork unitOfWork,
+        IMapper mapper
+        )
     {
         _logger = logger;
         _dayScheduleRepository = dayScheduleRepository;
         _lessonEntityRepository = lessonEntityRepository;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
-    public Task Handle(DayScheduleDTO request, CancellationToken cancellationToken)
+    public async Task Handle(DayScheduleDTO request, CancellationToken cancellationToken)
     {
         _logger.LogInformation("broker continue");
-        return Task.CompletedTask;
+
+        await _unitOfWork.CommitAsync(cancellationToken);
+        var groupEntity = _mapper.Map<DayScheduleEntity>(request);
+        var lessonsEntity = _mapper.Map<List<LessonEntity>>(request.Lessons);
+
+        lessonsEntity.ForEach(c => c.Groupid = groupEntity.Id);
+        await _dayScheduleRepository.AddAsync(groupEntity);
+
+        foreach (var lessonEntity in lessonsEntity)
+        {
+            await _lessonEntityRepository.AddAsync(lessonEntity);
+        }
+
+        try
+        {
+          await _unitOfWork.CommitAsync(cancellationToken);
+        }
+        catch (CustomDbException ex) 
+        {
+            _logger.LogError(ex, "message to broker");
+            throw;
+        }
+
     }
 }
