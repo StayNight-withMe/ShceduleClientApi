d6eff06858725a61f73c3e1de55d94c41a9662dc
1111-ssss <gurskievgeni@gmail.com>
2026-03-17 07:16:15 +0300

    feat(getAllTeacher): add getAllTeacher implementation

--- src/Application/Features/AllTeacher/Queries/GetAllTeacherHandler.cs
+++ src/Application/Features/AllTeacher/Queries/GetAllTeacherHandler.cs
@@ -1,17 +1,30 @@
-﻿
-
+﻿using Application.Abstraction.DataBase;
 using Application.Features.GeneralData;
+using Domain.Model.Entity;
 using Domain.Model.ReturnEntity;
+using Domain.Specification;
 using MediatR;
 
 namespace Application.Features.AllTeacher.Queries;
 public class GetAllTeacherHandler : IRequestHandler<GetAllTeacherQuery, TResult<List<string>>>
 {
-    public Task<TResult<List<string>>> Handle(
+    private readonly IBaseRepository<TeacherEntity> _teacherRepository;
+
+    public GetAllTeacherHandler(
+        IBaseRepository<TeacherEntity> teacherRepository
+    )
+    {
+        _teacherRepository = teacherRepository;
+    }
+
+    public async Task<TResult<List<string>>> Handle(
         GetAllTeacherQuery request,
         CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var teacherList = await _teacherRepository.ListAsync(new GetOrderedTeacherSpec(), ct);
 
+        return TResult<List<string>>.CompletedOperation(
+            teacherList ?? new()
+        );
     }
 }
