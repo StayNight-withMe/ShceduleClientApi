2a49e75f7ff6142f0d5838aae72474997ac2ab9c
1111-ssss <gurskievgeni@gmail.com>
2026-03-21 06:41:25 +0300

    fix(getGroupWeek): fix common dto field

---
+++ src/Application/Features/GroupWeekSchedule/Common/GetGroupWeekScheduleDTO.cs
@@ -0,0 +1,8 @@
+﻿using Contracts.Schedules;
+
+namespace Application.Features.TeacherSchedule.Common;
+
+public class GetGroupWeekScheduleDTO
+{
+    public List<DayScheduleDTO> Schedule { get; set; } = default!;
+}
