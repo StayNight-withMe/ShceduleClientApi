9782787148539945b1cead518d44d38aded36743
Виктор <kasa.king_over@mail.ru>
2026-05-08 17:33:38 +0300

    feat: hot add GetTeacherWeekScheduleHandler

--- src/Application/Features/FullWeekSchedule/Queries/GetTeacherWeekScheduleHandler.cs
+++ src/Application/Features/FullWeekSchedule/Queries/GetTeacherWeekScheduleHandler.cs
@@ -1,10 +1,50 @@
 ﻿using Application.Features.TeacherSchedule.Common;
+using Application.Abstraction.DataBase;
 using Domain.Model.ReturnEntity;
+using Domain.Model.Entity;
+using Domain.Specification;
+using Contracts.Schedules;
+using Contracts.Common;
 using MediatR;
 
 namespace Application.Features.TeacherSchedule.Queries;
 
 public class GetFullWeekScheduleHandler : IRequestHandler<GetFullWeekScheduleQuery, TResult<GetFullWeekScheduleDTO>>
 {
-    public Task<TResult<GetFullWeekScheduleDTO>> Handle(GetFullWeekScheduleQuery request, CancellationToken cancellationToken) => throw new NotImplementedException();
+    private readonly IBaseRepository<LessonEntity> _repository;
+
+    public GetFullWeekScheduleHandler(IBaseRepository<LessonEntity> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<TResult<GetFullWeekScheduleDTO>> Handle(GetFullWeekScheduleQuery request, CancellationToken cancellationToken)
+    {
+        var spec = new GetFullWeekScheduleSpec(request.date);
+        var lessons = await _repository.ListAsync(spec, cancellationToken);
+
+        // 2. Группируем занятия по группам
+        var schedules = lessons
+            .GroupBy(l => l.daySchedule.GroupName)
+            .Select(g => new DayScheduleDTO
+            {
+                Group = g.Key,
+                Date = request.date,
+                Lessons = g.Select(l => new Lesson
+                {
+                    StartTime = l.StartTime, 
+                    EndTime = l.EndTime,     
+                    Lesson1 = l.Subject1,    
+                    Lesson2 = l.Subject2,
+                    Fio1 = l.Teacher1,      
+                    Fio2 = l.Teacher2,
+                    ClassRoom1 = l.Classroom1, 
+                    ClassRoom2 = l.Classroom2
+                }).ToList()
+            })
+            .ToList();
+
+        var resultDto = new GetFullWeekScheduleDTO() { Shcedule = schedules };
+        return TResult<GetFullWeekScheduleDTO>.CompletedOperation(resultDto);
+    }
 }
---
+++ src/Domain/Specification/GetFullWeekScheduleSpec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Ardalis.Specification;
+using Domain.Model.Entity;
+
+namespace Domain.Specification;
+
+public class GetFullWeekScheduleSpec : Specification<LessonEntity>
+{
+    public GetFullWeekScheduleSpec(DateOnly date)
+    {
+        Query
+            .AsNoTracking()
+            .Where(x => x.daySchedule.Date == date)
+            .Include(x => x.daySchedule)
+            .OrderBy(x => x.daySchedule.GroupName) 
+            .ThenBy(x => x.LessonNumber); 
+    }
+}
